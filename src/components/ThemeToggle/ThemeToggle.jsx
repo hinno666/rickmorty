5184b10ccbfd525e9de5c4e5c87bb47ea2997b06
Andrew Karpov <luffytaroyo@gmail.com>
2023-10-12 12:40:43 +0300

    added toggle theme/create any components with css styles

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,5 +1,35 @@
+import { useGlobalContext } from "../context/context"
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs'
+
+
+import './ThemeToggle.css'
+
 export const ThemeToggle = () => {
+
+  const { isDarkMode, toggleDarkTheme } = useGlobalContext()
+
   return (
-    <div>ThemeToggle</div>
+    <section className="toggle-theme">
+      <div className="logo">
+        <h1>
+          <span>r</span>
+          <span>i</span>
+          <span>c</span>
+          <span>k</span>
+          <span>m</span>
+          <span>o</span>
+          <span>r</span>
+          <span>t</span>
+          <span>y</span>
+        </h1>
+      </div>
+      <button className="toggle-theme-btn" onClick={toggleDarkTheme}>
+        {isDarkMode ? (
+          <BsFillSunFill className="toggle-icon" />
+        ) : (
+          <BsFillMoonFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
   )
 }
